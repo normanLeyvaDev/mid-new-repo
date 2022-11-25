b208ec635c03d126dc8a3c0652d981c746ce7460
Norman Leyva <nleyvagarcia@gmail.com>
2022-11-25 12:40:16 +0100

    update mid

--- scripts/api-get.js
+++ scripts/api-get.js
@@ -4,30 +4,31 @@ const getExternalAPiInfo = () => {
     .then((res) => {
       let externalApiInfo = "";
 
+      // externalApiInfo += `
+      //           <div class="card" style="width: 15rem">
+      //             <div class="card-body">
+      //                 <h5 class="card-title">${res[0].title}</h5>
+      //                 <p class="card-text">
+      //                     ${res[0].body}
+      //                 </p>
+      //             </div>
+      //           </div>
+      //           `;
+
       externalApiInfo += `
-                <div class="card" style="width: 15rem">
-                  <div class="card-body">
-                      <h5 class="card-title">${res[0].title}</h5>
-                      <p class="card-text">
-                          ${res[0].body}
-                      </p>
-                  </div>
-                </div>
-                `;
+          <div class="project-card-title">${res[0].title}</div>
+          <div class="project-card-subtitle">${res[0].body}</div>
+          <a href="./simplify.html" target="_blank" class="project-link">Learn More</a>
+      `;
 
       document.querySelector("#output1").innerHTML = externalApiInfo;
 
       externalApiInfo = "";
 
       externalApiInfo += `
-                  <div class="card" style="width: 15rem">
-                    <div class="card-body">
-                        <h5 class="card-title">${res[1].title}</h5>
-                        <p class="card-text">
-                            ${res[1].body}
-                        </p>
-                    </div>
-                  </div>
+      <div class="project-card-title">${res[1].title}</div>
+      <div class="project-card-subtitle">${res[1].body}</div>
+      <a href="./simplify.html" target="_blank" class="project-link">Learn More</a>
                   `;
 
       document.querySelector("#output2").innerHTML = externalApiInfo;
@@ -35,24 +36,20 @@ const getExternalAPiInfo = () => {
       externalApiInfo = "";
 
       externalApiInfo += `
-                  <div class="card" style="width: 15rem">
-                    <div class="card-body">
-                        <h5 class="card-title">${res[2].title}</h5>
-                        <p class="card-text">
-                            ${res[2].body}
-                        </p>
-                    </div>
-                  </div>
+      <div class="project-card-title">${res[2].title}</div>
+      <div class="project-card-subtitle">${res[2].body}</div>
+      <a href="./simplify.html" target="_blank" class="project-link">Learn More</a>
                   `;
 
       document.querySelector("#output3").innerHTML = externalApiInfo;
     })
     .catch((error) => console.log(error));
 };
-document
-  .querySelector(".project-link")
-  .addEventListener("click", getExternalAPiInfo());
+// document
+//   .querySelector(".project-link")
+//   .addEventListener("click", getExternalAPiInfo());
 
+getExternalAPiInfo();
 /* enviar codigo */
 
-prevent.preventDefault();
+/*prevent.preventDefault();*/
